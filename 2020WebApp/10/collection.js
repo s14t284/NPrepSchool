fc1a16834676b4bb60e01bc79a975cf691b79d83
Ryuya Ikeda <rikeda71@gmail.com>
2020-04-14 23:54:00 +0900

    minor fix and add setting files

--- 2020WebApp/10/collection.js
+++ 2020WebApp/10/collection.js
@@ -1,7 +1,7 @@
 let classes = ["A", "B", "C", "D"];
 
 for (let grade = 0; grade < 4; grade++) {
-  for (let i = 0; i < classes.length; i++) {
+  for (let i = 1; i <= classes.length; i++) {
     document.write("<p>" + grade + "年" + classes[i] + "</p>");
   }
 }
